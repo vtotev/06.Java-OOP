8a9daf2e0225102e8b532312fabb8d3a3f1364e1
Valeri Totev <valerivt@yahoo.com>
2021-06-04 08:24:34 +0300

    add

---
+++ 01.Working-with-Abstraction/RhombusOfStars/Main.java
@@ -0,0 +1,25 @@
+package WorkingWithAbstraction.RhombusOfStars;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size = Integer.parseInt(scan.nextLine());
+        for (int starCount = 1; starCount <= size; starCount++) {
+            printRow(size, starCount);
+        }
+        for (int starCount = size - 1; starCount >= 1; starCount--) {
+            printRow(size, starCount);
+        }
+    }
+
+    private static void printRow(int figureSize, int starCount) {
+        for (int i = 0; i < figureSize - starCount; i++)
+            System.out.print(" ");
+        for (int col = 1; col < starCount; col++) {
+            System.out.print("* ");
+        }
+        System.out.println("*");
+    }
+}
