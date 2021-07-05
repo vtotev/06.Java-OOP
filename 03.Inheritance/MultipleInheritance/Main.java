00bde0c8cba6110b6953e9f6850ee96286821b5d
Valeri Totev <valerivt@yahoo.com>
2021-07-05 21:08:37 +0300

    add

---
+++ 03.Inheritance/MultipleInheritance/Main.java
@@ -0,0 +1,10 @@
+package Inheritance.MultipleInheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Puppy dog = new Puppy();
+        dog.eat();
+        dog.bark();
+        dog.weep();
+    }
+}
