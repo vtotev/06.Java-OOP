50c7f9380957f255274eea4fd8a88f041cacf928
Valeri Totev <valerivt@yahoo.com>
2021-07-14 19:56:50 +0300

    add

---
+++ 03.Inheritance/PlayersAndMonsters/SoulMaster.java
@@ -0,0 +1,7 @@
+package Inheritance.PlayersAndMonsters;
+
+public class SoulMaster extends DarkWizard{
+    public SoulMaster(String username, int level) {
+        super(username, level);
+    }
+}
