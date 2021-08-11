a69b35b94ba94b4ac2dadc78a4d474393fa9c20e
Valeri Totev <valerivt@yahoo.com>
2021-08-11 12:22:33 +0300

    add

---
+++ 04.Interfaces-and-Abstraction/SayHelloExtended/European.java
@@ -0,0 +1,13 @@
+package InterfacesAndAbstraction.SayHelloExtended;
+
+public class European extends BasePerson implements Person {
+
+    public European(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Hello";
+    }
+}
